b0235976c676431cbea893b996241493ec01f70e
Nicholas Jakobsen <nicholas.jakobsen@telus.net>
2013-02-24 20:40:04 -0800

    Included images and fixed edit_page button not including the return to address.

--- app/helpers/editable_pages/pages_helper.rb
+++ app/helpers/editable_pages/pages_helper.rb
@@ -1,7 +1,7 @@
 module EditablePages
   module PagesHelper
     def edit_page_link(page_title, html_options = {})
-      link_to 'Edit Page', edit_editable_pages_page_path(Page.find!(page_title)), html_options
+      link_to 'Edit Page', edit_editable_pages_page_path(Page.find!(page_title), :return_to => url_for(params)), html_options
     end  
 
     # Renders the contents of an editable section
